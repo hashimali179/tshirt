bc11177fe69e4fb2bc4a6aba003b1b454df268d1
Hashim Ali Zaidi <hashimali179@gmail.com>
2023-10-23 11:16:13 +0530

    first push

---
+++ Final-SIngle-Image.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+def main():
+    st.title("Image Background Replacement")
+
+    # Upload background and foreground images
+    background_image = st.file_uploader("Upload Background Image", type=["jpg", "png", "jpeg"])
+    foreground_image = st.file_uploader("Upload Foreground Image", type=["jpg", "png", "jpeg"])
+
+    # Add sliders in the sidebar
+    fg_height = st.sidebar.slider("Foreground Height", min_value=10, max_value=500, value=100)
+    fg_width = st.sidebar.slider("Foreground Width", min_value=10, max_value=500, value=100)
+    x_fg_offset = st.sidebar.slider("X FG Offset", min_value=0, max_value=300, value=0)
+    y_fg_offset = st.sidebar.slider("Y FG Offset", min_value=0, max_value=300, value=0)
+
+    
+    if background_image and foreground_image:
+        # Read the uploaded images using OpenCV
+        bg_image = cv2.imdecode(np.fromstring(background_image.read(), np.uint8), 1)
+        fg_image = cv2.imdecode(np.fromstring(foreground_image.read(), np.uint8), 1)
+
+
+        # Resize the foreground image based on slider values
+        resized_fg_image = cv2.resize(fg_image, (fg_width, fg_height))
+
+        # Calculate the position to place the resized foreground image based on slider values
+        y_offset = y_fg_offset
+        x_offset = x_fg_offset
+        
+
+        # Replace the background with the foreground on the specified location
+        bg_image[y_offset:y_offset + fg_height, x_offset:x_offset + fg_width] = resized_fg_image
+
+            
+        # Display the result
+        st.image(bg_image, channels="BGR", use_column_width=True, caption="Result")
+
+if __name__ == "__main__":
+    main()
---
+++ text.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+def main():
+    st.title("Image Text Placement with OpenCV")
+
+    # Upload an image
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+    # Add text parameters in the sidebar
+    text = st.sidebar.text_input("Text to add", "Hello, Streamlit!")
+    text_color_r = st.sidebar.slider("Text Color (Red)", min_value=0, max_value=255, value=255)
+    text_color_g = st.sidebar.slider("Text Color (Green)", min_value=0, max_value=255, value=255)
+    text_color_b = st.sidebar.slider("Text Color (Blue)", min_value=0, max_value=255, value=255)
+    font_size = st.sidebar.slider("Font Size", min_value=10, max_value=100, value=36)
+    thickness = st.sidebar.slider("Thickness", min_value=1, max_value=10, value=2)
+    x_center_position_percent = st.sidebar.slider("X Center Position (%)", min_value=0, max_value=100, value=50)
+    y_position_percent = st.sidebar.slider("Y Position (%)", min_value=0, max_value=100, value=50)
+
+    if uploaded_image:
+        # Read the uploaded image using OpenCV
+        img = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+
+        # Convert BGR image to RGB
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Define the font
+        font = cv2.FONT_HERSHEY_PLAIN
+
+        # Calculate the text size
+        text_size = cv2.getTextSize(text, font, font_size / 10, thickness)[0]
+
+        # Calculate the position as percentages of the image size
+        image_height, image_width, _ = img_rgb.shape
+        x_position = int((x_center_position_percent / 100) * image_width - (text_size[0] // 2))
+        y_position = int((y_position_percent / 100) * image_height)
+
+        # Set the font color and thickness
+        color = (text_color_r, text_color_g, text_color_b)
+
+        # Convert font size to OpenCV-compatible integer
+        font_size = int(font_size)
+
+        # Add the text to the image
+        cv2.putText(img_rgb, text, (x_position, y_position), font, font_size / 10, color, thickness)
+
+        # Display the image with the added text
+        st.image(img_rgb, channels="RGB", use_column_width=True, caption="Image with Text")
+
+if __name__ == "__main__":
+    main()
